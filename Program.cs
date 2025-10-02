4798248cba8c3133a6ff1200edc7d6ae2dbeddae
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2025-10-02 15:00:03 -0700

    Add DomainHint to auth config

--- Program.cs
+++ Program.cs
@@ -7,7 +7,33 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
+    .EnableTokenAcquisitionToCallDownstreamApi()
+    .AddInMemoryTokenCaches();
+
+// Configure OpenID Connect options to add domain hint
+builder.Services.Configure<OpenIdConnectOptions>(OpenIdConnectDefaults.AuthenticationScheme, options =>
+{
+    options.Events = new OpenIdConnectEvents
+    {
+        OnRedirectToIdentityProvider = context =>
+        {
+            // Add domain hint to redirect to UC Davis login
+            context.ProtocolMessage.DomainHint = "ucdavis.edu";
+            
+            // Optional: Add login hint with a specific user format
+            // context.ProtocolMessage.LoginHint = "user@ucdavis.edu";
+            
+            // Optional: Force re-authentication
+            // context.ProtocolMessage.Prompt = "login";
+            
+            // Optional: Set specific scopes
+            // context.ProtocolMessage.Scope = "openid profile email";
+            
+            return Task.CompletedTask;
+        }
+    };
+});
 
 builder.Services.AddRazorPages(options =>
 {
